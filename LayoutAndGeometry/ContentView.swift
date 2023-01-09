b5c0d09d35cb1f4037e07117acd212fa85b3333e
pat <88806542+patgd@users.noreply.github.com>
2023-01-09 10:08:37 -1000

    Added opacity fade out at top of scroll.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
                     GeometryReader { geo in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 1, y: 0, z: 0))
-                            .frame(width: 200, height: 200)
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(geo.frame(in: .global).minY < 200 ? geo.frame(in: .global).minY / 200 - 0.15 : 1)
                     }
-                    .frame(width: 200, height: 200)
+                    .frame(height: 40)
                 }
             }
         }
